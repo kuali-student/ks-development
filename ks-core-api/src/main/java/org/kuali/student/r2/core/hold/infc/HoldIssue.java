0979274bca754f37559fd2dc82ebd58676d6d4b7
mezba.mahtab <mezba.mahtab@kuali.org>
2014-08-01 15:20:35 -0400

    KSENROLL-13922 Updated HoldIssue
    git-svn-id: https://svn.kuali.org/repos/student/enrollment/ks-api/trunk@73229 0ac02d80-fb3f-11dc-a574-c57244a850eb

--- ks-core-api/src/main/java/org/kuali/student/r2/core/hold/infc/HoldIssue.java
+++ ks-core-api/src/main/java/org/kuali/student/r2/core/hold/infc/HoldIssue.java
@@ -17,6 +17,8 @@ package org.kuali.student.r2.core.hold.infc;
 
 import org.kuali.student.r2.common.infc.IdEntity;
 
+import java.util.Date;
+
 
 /**
  * Information about an Issue.
@@ -34,4 +36,44 @@ public interface HoldIssue
      * @required
      */
     public String getOrganizationId();
+
+    /**
+     * The flag that stores if the HoldIssue is a Term+Date based.
+     */
+    public Boolean getIsHoldIssueTermAndDateBased ();
+
+    /**
+     * The flag that stores if the HoldIssue is only Date-based.
+     */
+    public Boolean getIsHoldIssueDateBasedOnly ();
+
+    /**
+     * Gets the first applied date as of which this HoldIssue can be applied to students.
+     */
+    public Date getFirstAppliedDate ();
+
+    /**
+     * Gets the last applied date as of which this HoldIssue can no longer be applied to students.
+     */
+    public Date getLastAppliedDate ();
+
+    /**
+     * Gets the id of the first application term as of which this (Term-based) HoldIssue may be applied to students.
+     */
+    public String getFirstApplicationTermId();
+
+    /**
+     * Gets the id of the last application term as of which this (Term-based) HoldIssue may no longer be applied to students.
+     */
+    public String getLastApplicationTermId();
+
+    /**
+     * Gets the flag that specifies whether or not a history the application of this Hold to a student is to be maintained.
+     */
+    public Boolean getMaintainHistoryOfApplicationOfHold ();
+
+    /**
+     * Gets the Hold Code (the public identifier for the Hold) for the HoldIssue.
+     */
+    public String getHoldCode ();
 }
